60c70d67bba87860af0172e4e95a98d1e2c1c55f
a-mahir-d <amd.coding.ac@gmail.com>
2026-06-01 11:24:11 +0300

    typo fix

--- src/frontend/src/environments/environment.ts
+++ src/frontend/src/environments/environment.ts
@@ -1,8 +1,8 @@
 import { dev } from './dev';
 import { prod } from './prod';
 
-// const env = dev;
-const env = prod;
+// const env = prod;
+const env = dev;
 
 export const environment = {
   serverUrl: prod.serverUrl
